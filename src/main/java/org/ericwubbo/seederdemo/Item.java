a3efb14e7b8af142678d9d8a8057868f1505b653
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-12 10:38:37 +0200

    removed Setter

--- src/main/java/org/ericwubbo/seederdemo/Item.java
+++ src/main/java/org/ericwubbo/seederdemo/Item.java
@@ -1,9 +1,10 @@
 package org.ericwubbo.seederdemo;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 
